5fd85e83cd4efa673e1959d9e440b4fd56200323
Anne-M <77175884+annemarieb78@users.noreply.github.com>
2024-07-17 12:38:42 +0200

    Tidy up

--- js/script.js
+++ js/script.js
@@ -27,7 +27,6 @@ getWord();
 const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
-    // console.log(letter);
     placeholderLetters.push("●");
   }
   wordInProgress.innerText = placeholderLetters.join("");
@@ -41,7 +40,6 @@ guessLetterButton.addEventListener("click", function (e) {
   if (goodGuess) {
     makeGuess(guess);
   }
-  //console.log(guess)
   letterInput.value = "";
 });
 
